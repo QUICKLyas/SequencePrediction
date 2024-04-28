b6333fc5d8441108954666272ea7463ce02e3557
ARKI <acemargatroid@qq.com>
2024-04-28 19:33:03 +0800

    import mongo-c-driver & mongo-cxx-driver

--- cppcompro/src/main.cpp
+++ cppcompro/src/main.cpp
@@ -2,25 +2,16 @@
 
 using namespace std;
 
-//#include <bsoncxx/json.hpp>
-//#include <mongocxx/client.hpp>
-//#include <mongocxx/instance.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include "util/mongoc-cxx.hpp"
 int main()
 {
-
+    MongoC *mongoC = new MongoC("root","345789@Quickly");
+    mongoC->connectDB("gamble");
 }
 
-//// create an instance
-//mongocxx::instance inst{};
-//// set uri
-//const auto uri = mongocxx::uri{"mongodb://root:345789%40Quickly@localhost:27017"};
-//// set the version of the Stable API on the client
-//mongocxx::options::client client_options;
-//const auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
-//client_options.server_api_opts(api);
-//// Set up the connection and get a handle on the database
-//mongocxx::client conn{uri, client_options};
-//for (auto item : conn.list_database_names()) {
-//cout << item << endl;
+
 //}
 //return 0;
--- cppcompro/src/util/mongoc-cxx.hpp
+++ cppcompro/src/util/mongoc-cxx.hpp
@@ -8,6 +8,8 @@
 #include <mongoc.h>
 #include <string>
 #include <iostream>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
 
 using namespace std;
 // abstract function
@@ -70,6 +72,7 @@ int MongoC::setClient () {
         rc = 1;
         cleanUp();
     }
+    return rc;
 }
 // connect database
 int MongoC::connectDB(const char *dbName) {
@@ -122,4 +125,30 @@ void Datagram::printStr(){
               << "command " << this->command << "," << endl
               << "}" << std::endl;
 }
+
+class MongoCXX{
+private:
+    mongocxx::instance inst{};
+    const mongocxx::uri uri;
+public:
+    int cleanUp();
+    int setClient();
+    int connectDB(const char *dbName);
+    MongoCXX(string userName, string passwd) {
+
+    }
+};
+//// create an instance
+//mongocxx::instance inst{};
+//// set uri
+//const auto uri = mongocxx::uri{"mongodb://root:345789%40Quickly@localhost:27017"};
+//// set the version of the Stable API on the client
+//mongocxx::options::client client_options;
+//const auto api = mongocxx::options::server_api{mongocxx::options::server_api::version::k_version_1};
+//client_options.server_api_opts(api);
+//// Set up the connection and get a handle on the database
+//mongocxx::client conn{uri, client_options};
+//for (auto item : conn.list_database_names()) {
+//cout << item << endl;
+//}
 #endif //WEBREP_PY_MONGOC_CXX_H
