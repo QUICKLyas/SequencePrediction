20621c9cb5c6221b7aff7ee3672d461eac14bc63
ARKI <acemargatroid@qq.com>
2024-05-06 13:49:18 +0800

    start owner-timer

--- cppcompro/src/main.cpp
+++ cppcompro/src/main.cpp
@@ -1,22 +1,8 @@
+#include <owner-timer.h>
 #include <iostream>
-#include "user.h"
 using namespace std;
-#include "mongo-con.hpp"
-#include "mongo-op.hpp"
 int main()
 {
-//    mongocxx::instance instance{};
-//    mongocxx::client client{mongocxx::uri{"mongodb://root:345789%40Quickly@localhost:27017"}};
-//    mongocxx::database db = client.database("gamble");
-//    mongocxx::collection col = db.collection("issue");
-    MonCXX monCxx = MonCXX();
-    MonCxxOP monCxxOp = MonCxxOP();
-    monCxx.connectDB();
-    collection * col = monCxx.connectCol();
-    monCxxOp.findSingleDoc(*col);
-
+    getUserName();
+    return 0;
 }
-
-
-//}
-//return 0;
--- cppcompro/src/util/owner-timer.h
+++ cppcompro/src/util/owner-timer.h
@@ -5,4 +5,42 @@
 #ifndef WEBREP_PY_OWNER_TIMER_H
 #define WEBREP_PY_OWNER_TIMER_H
 
+#include <string>
+#include <iostream>
+using namespace std;
+#ifdef linux
+#include <unistd.h>
+#include <pwd.h>
+#endif
+// now only for windows
+#ifdef _WIN64
+#include<Windows.h>
+#endif
+// abstract function
+string getPcName();
+void getUserName();
+
+string getPcName() {
+
+#if defined _WIN64
+    const int MAX_LEN = 4;
+    char szBuffer[MAX_LEN];
+    DWORD len = MAX_LEN;
+    GetComputerNameA(szBuffer, & len);
+#else
+    szBuffer = "";
+#endif
+    cout << szBuffer;
+}
+void getUserName() {
+#if defined _WIN64
+    const int MAX_LEN = 255;
+    char szBuffer[MAX_LEN];
+    DWORD len = MAX_LEN;
+    GetUserName(szBuffer, &len);
+#else // outher system
+    szBuffer = "";
+#endif
+    cout << szBuffer ;
+}
 #endif //WEBREP_PY_OWNER_TIMER_H
